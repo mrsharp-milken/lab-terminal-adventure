0c5ced5f0681f3439e21c02108e7f61fd7dc3474
mrsharp-milken <144956668+mrsharp-milken@users.noreply.github.com>
2024-02-09 12:06:01 -0800

    Update and rename welcomeToSurface.js to welcomeToSurface.py

--- welcomeToSurface.js
+++
@@ -1,4 +0,0 @@
-console.log("Welcome to the terminal adventure lab!");
-console.log("Whenever you see a .py file, use python3 followed by a space followed by the file name");
-console.log("Whenever you see a .js file, use node followed by a space followed by the file name");
-console.log("Now, type cd adventure to go beneath the surface and begin.");
---
+++ welcomeToSurface.py
@@ -0,0 +1,3 @@
+print("Welcome to the terminal adventure lab!");
+print("Whenever you see a .py file, use python3 followed by a space followed by the file name");
+print("Now, type cd adventure to go beneath the surface and begin.");
